33767e645ae989ecf0918c77258dc4745bedd670
lalu mutawalli <laluallistilo@gmail.com>
2022-07-08 04:36:56 -0700

    load latihan django apps ToDo

---
+++ website/todo/models.py
@@ -0,0 +1,10 @@
+from distutils.text_file import TextFile
+from turtle import title
+from django.db import models
+
+class Todo(models.Model):
+    title = models.TextField(max_length=200)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s: %s' % (self.id, self.title)
